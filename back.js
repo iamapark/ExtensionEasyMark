9356d9aba103d2d7ca232b03c90fbbbaa6ff67c5
iamapark <iamapark89@gmail.com>
2013-05-11 15:42:41 +0900

    first add

---
+++ back.js
@@ -0,0 +1,35 @@
+var userCheckResult = function(data){
+	if(data.result == 'true'){ // 로그인 성공
+		console.log('로그인 성공');
+		$('#main').show();
+		$('#loginForm').hide();
+	}else{ // 로그인 실패
+		console.log('로그인 실패');
+	}
+};
+
+$('#loginButton').click(function(){
+	 userId = $('#userId').val();
+	 password = $('#password').val();
+	 $.ajax({
+	    url : "http://localhost:8080/EasyMark/extensionUserCheck",
+	    dataType : "jsonp",
+	    jsonp : "userCheckResult",
+	    jsonpCallback:"userCheckResult",
+	    crossDomain: true,
+	    data : {
+	        userId: userId,
+	        password: password
+	    }
+	});
+  });
+
+$('#addBookMark').click(function(){
+	alert('addBookMark');
+});
+
+$('#moveBookMark').click(function(){
+	alert('moveBookMark');
+});
+
+$('#main').hide();
